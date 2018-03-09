18f44778674b6993ab670e8a791ddf05fae533f9
Tim Beck <socaltdb@gmail.com>
2018-03-09 12:20:23 -0800

    restate length boolean

--- bruteforcer.py
+++ bruteforcer.py
@@ -6,8 +6,10 @@ class Guesser:
     def bruteWordlist(self, dictionary, reader, minLength=1, maxLength=0):
     	currentLine = 1
     	for line in dictionary.readlines():
-            if (minLength <= len(line.replace("\n",""))) and ((maxLength >= len(line.replace("\n","")) or maxLength == None)):
-        		sys.stdout.write("\r\033[K"+"Attempt "+str(currentLine)+", trying: " + str(line.replace("\n","")))
+            if (len(line.replace("\n","")) >= minLength or minLength == 0) and \
+                (len(line.replace("\n","")) <= maxLength or maxLength == 0):
+        		sys.stdout.write("\r\033[K"+"Attempt "+str(currentLine)+
+                    ", trying: " + str(line.replace("\n","")))
         		sys.stdout.flush()
         		if reader.decrypt(line.replace("\n","")) == 1:
         			print("\nSuccess! The password was: " + line)
