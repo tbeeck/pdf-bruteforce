4e7c876c1fd78de10c0747bf2233a0533db8c58d
Tim Beck <socaltdb@gmail.com>
2018-03-09 12:01:41 -0800

    enable length designation in wordlist function

--- bruteforcer.py
+++ bruteforcer.py
@@ -3,16 +3,16 @@ import PyPDF2
 
 class Guesser:
 
-    def bruteWordlist(self, dictionary, reader):
+    def bruteWordlist(self, dictionary, reader, minLength=1, maxLength=0):
     	currentLine = 1
     	for line in dictionary.readlines():
-    		sys.stdout.write("\r\033[K"+"Attempt "+str(currentLine)+", trying: "
-              + str(line.replace("\n","")))
-    		sys.stdout.flush()
-    		if reader.decrypt(line.replace("\n","")) == 1:
-    			print("\nSuccess! The password was: " + line)
-    			return
-    		else:
-    			currentLine = currentLine + 1
-    	print("\nThe password is not in the wordlist :(")
+            if (minLength <= len(line.replace("\n",""))) and ((maxLength >= len(line.replace("\n","")) or maxLength == None)):
+        		sys.stdout.write("\r\033[K"+"Attempt "+str(currentLine)+", trying: " + str(line.replace("\n","")))
+        		sys.stdout.flush()
+        		if reader.decrypt(line.replace("\n","")) == 1:
+        			print("\nSuccess! The password was: " + line)
+        			return
+        		else:
+        			currentLine = currentLine + 1
+    	print("\nThe password is not in the wordlist.")
     	return
